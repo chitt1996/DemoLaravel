1a3cd2d6e57c9e132847aff88b89c516b4a6b7ee
chitt1996 <51982987+chitt1996@users.noreply.github.com>
2019-10-24 23:59:02 +0700

    layout, route complete

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,21 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::group(['prefix' => 'category'], function() {
+        Route::get('list-category','CategoryController@index');
+        Route::get('list-category/create','CategoryController@create');
+        Route::get('list-category/edit/{id}','CategoryController@edit');
+    });
+    Route::group(['prefix' => 'user'], function() {
+        Route::get('list-user','UserController@index');
+        Route::get('list-user/create','UserController@create');
+        Route::get('list-user/edit/{id}','UserController@edit');
+    });
+    Route::group(['prefix' => 'product'], function() {
+        Route::get('list-product','ProductController@index');
+        Route::get('list-product/create','ProductController@create');
+        Route::get('list-product/edit/{id}','ProductController@edit');
+    });
 });
